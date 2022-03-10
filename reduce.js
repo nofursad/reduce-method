6402885fdeae83bb1bf4f7a1237f480665f9bd60
Abhisek Joshi <nofursad@gmail.com>
2022-03-10 22:23:05 +0000

    Add example

--- reduce.js
+++ reduce.js
@@ -55,3 +55,14 @@ let experienceByProfession = teamMembers.reduce((acc, curr) => {
   return acc;
 }, {});
 console.log(experienceByProfession);
+
+// let nameByProfession = teamMembers.reduce((acc, curr) => {
+//   let key = curr.profession;
+//   if(!acc[key]){
+//     acc[key] = curr.name;
+//   } else {
+//     acc[key].push(curr.name);
+//   }
+//   return acc;
+// }, {});
+// console.log(nameByProfession);
